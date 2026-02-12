89c1ba6e0ec6d8dbbe1fb57d59f54e435f262d2d
Mushtaq Ahamad <mohammadmushtaq1295@gmail.com>
2026-02-12 23:33:12 +0530

    feat: add react-hot-toast for notifications and integrate Toaster component in main application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
+     <Toaster position="top-right" />
   </AuthProvider>
 );
